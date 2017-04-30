5cec513975309f3d9d57f233695ab590c2a1a370
Matthew Bulger <mcbulger2@gmail.com>
2017-04-30 09:21:10 -0600

    Whole program.

---
+++ GraphingCalc/GraphingCalc/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace GraphingCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput1 = new System.Windows.Forms.TextBox();
+            this.btnGraph = new System.Windows.Forms.Button();
+            this.lblY1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput2 = new System.Windows.Forms.TextBox();
+            this.btnColor1 = new System.Windows.Forms.Button();
+            this.btnColor2 = new System.Windows.Forms.Button();
+            this.nudGridSize = new System.Windows.Forms.NumericUpDown();
+            this.cbShowGrid = new System.Windows.Forms.CheckBox();
+            this.nudThickness = new System.Windows.Forms.NumericUpDown();
+            this.nudSampleRate = new System.Windows.Forms.NumericUpDown();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.btnColor3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInput3 = new System.Windows.Forms.TextBox();
+            this.nudRange = new System.Windows.Forms.NumericUpDown();
+            this.cbRangeAuto = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGridSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampleRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRange)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbInput1
+            // 
+            this.tbInput1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput1.Location = new System.Drawing.Point(872, 12);
+            this.tbInput1.Name = "tbInput1";
+            this.tbInput1.Size = new System.Drawing.Size(100, 20);
+            this.tbInput1.TabIndex = 0;
+            // 
+            // btnGraph
+            // 
+            this.btnGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGraph.Location = new System.Drawing.Point(897, 280);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(75, 23);
+            this.btnGraph.TabIndex = 1;
+            this.btnGraph.Text = "Graph";
+            this.btnGraph.UseVisualStyleBackColor = true;
+            this.btnGraph.Click += new System.EventHandler(this.btnGraph_Click);
+            // 
+            // lblY1
+            // 
+            this.lblY1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblY1.AutoSize = true;
+            this.lblY1.Location = new System.Drawing.Point(839, 15);
+            this.lblY1.Name = "lblY1";
+            this.lblY1.Size = new System.Drawing.Size(26, 13);
+            this.lblY1.TabIndex = 2;
+            this.lblY1.Text = "Y1=";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(839, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Y2=";
+            // 
+            // tbInput2
+            // 
+            this.tbInput2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput2.Location = new System.Drawing.Point(872, 38);
+            this.tbInput2.Name = "tbInput2";
+            this.tbInput2.Size = new System.Drawing.Size(100, 20);
+            this.tbInput2.TabIndex = 3;
+            // 
+            // btnColor1
+            // 
+            this.btnColor1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColor1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor1.Location = new System.Drawing.Point(813, 11);
+            this.btnColor1.Name = "btnColor1";
+            this.btnColor1.Size = new System.Drawing.Size(20, 20);
+            this.btnColor1.TabIndex = 5;
+            this.btnColor1.Tag = "0";
+            this.btnColor1.UseVisualStyleBackColor = true;
+            this.btnColor1.Click += new System.EventHandler(this.colorSelect);
+            // 
+            // btnColor2
+            // 
+            this.btnColor2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColor2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor2.Location = new System.Drawing.Point(813, 37);
+            this.btnColor2.Name = "btnColor2";
+            this.btnColor2.Size = new System.Drawing.Size(20, 20);
+            this.btnColor2.TabIndex = 6;
+            this.btnColor2.Tag = "1";
+            this.btnColor2.UseVisualStyleBackColor = true;
+            this.btnColor2.Click += new System.EventHandler(this.colorSelect);
+            // 
+            // nudGridSize
+            // 
+            this.nudGridSize.Location = new System.Drawing.Point(852, 610);
+            this.nudGridSize.Name = "nudGridSize";
+            this.nudGridSize.ReadOnly = true;
+            this.nudGridSize.Size = new System.Drawing.Size(120, 20);
+            this.nudGridSize.TabIndex = 7;
+            this.nudGridSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudGridSize.ValueChanged += new System.EventHandler(this.nudGridSize_ValueChanged);
+            // 
+            // cbShowGrid
+            // 
+            this.cbShowGrid.AutoSize = true;
+            this.cbShowGrid.Checked = true;
+            this.cbShowGrid.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbShowGrid.Location = new System.Drawing.Point(897, 577);
+            this.cbShowGrid.Name = "cbShowGrid";
+            this.cbShowGrid.Size = new System.Drawing.Size(51, 17);
+            this.cbShowGrid.TabIndex = 8;
+            this.cbShowGrid.Text = "show";
+            this.cbShowGrid.UseVisualStyleBackColor = true;
+            this.cbShowGrid.Click += new System.EventHandler(this.cbShowGrid_Click);
+            // 
+            // nudThickness
+            // 
+            this.nudThickness.Location = new System.Drawing.Point(852, 651);
+            this.nudThickness.Name = "nudThickness";
+            this.nudThickness.ReadOnly = true;
+            this.nudThickness.Size = new System.Drawing.Size(120, 20);
+            this.nudThickness.TabIndex = 9;
+            this.nudThickness.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.nudThickness.ValueChanged += new System.EventHandler(this.nudThickness_ValueChanged);
+            // 
+            // nudSampleRate
+            // 
+            this.nudSampleRate.DecimalPlaces = 2;
+            this.nudSampleRate.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.nudSampleRate.Location = new System.Drawing.Point(828, 148);
+            this.nudSampleRate.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudSampleRate.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.nudSampleRate.Name = "nudSampleRate";
+            this.nudSampleRate.ReadOnly = true;
+            this.nudSampleRate.Size = new System.Drawing.Size(120, 20);
+            this.nudSampleRate.TabIndex = 10;
+            this.nudSampleRate.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            65536});
+            this.nudSampleRate.ValueChanged += new System.EventHandler(this.nudSampleRate_ValueChanged);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(13, 14);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(35, 13);
+            this.lblX.TabIndex = 11;
+            this.lblX.Text = "label2";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(13, 41);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(35, 13);
+            this.lblY.TabIndex = 12;
+            this.lblY.Text = "label3";
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Location = new System.Drawing.Point(897, 320);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 13;
+            this.btnCenter.Text = "Center";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // btnColor3
+            // 
+            this.btnColor3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColor3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor3.Location = new System.Drawing.Point(813, 63);
+            this.btnColor3.Name = "btnColor3";
+            this.btnColor3.Size = new System.Drawing.Size(20, 20);
+            this.btnColor3.TabIndex = 16;
+            this.btnColor3.Tag = "2";
+            this.btnColor3.UseVisualStyleBackColor = true;
+            this.btnColor3.Click += new System.EventHandler(this.colorSelect);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(839, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Y3=";
+            // 
+            // tbInput3
+            // 
+            this.tbInput3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput3.Location = new System.Drawing.Point(872, 64);
+            this.tbInput3.Name = "tbInput3";
+            this.tbInput3.Size = new System.Drawing.Size(100, 20);
+            this.tbInput3.TabIndex = 14;
+            // 
+            // nudRange
+            // 
+            this.nudRange.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudRange.Location = new System.Drawing.Point(852, 815);
+            this.nudRange.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudRange.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudRange.Name = "nudRange";
+            this.nudRange.ReadOnly = true;
+            this.nudRange.Size = new System.Drawing.Size(120, 20);
+            this.nudRange.TabIndex = 17;
+            this.nudRange.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudRange.ValueChanged += new System.EventHandler(this.nudRange_ValueChanged);
+            // 
+            // cbRangeAuto
+            // 
+            this.cbRangeAuto.AutoSize = true;
+            this.cbRangeAuto.Location = new System.Drawing.Point(897, 792);
+            this.cbRangeAuto.Name = "cbRangeAuto";
+            this.cbRangeAuto.Size = new System.Drawing.Size(47, 17);
+            this.cbRangeAuto.TabIndex = 18;
+            this.cbRangeAuto.Text = "auto";
+            this.cbRangeAuto.UseVisualStyleBackColor = true;
+            this.cbRangeAuto.CheckedChanged += new System.EventHandler(this.autoRange);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGraph;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.cbRangeAuto);
+            this.Controls.Add(this.nudRange);
+            this.Controls.Add(this.btnColor3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbInput3);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.nudSampleRate);
+            this.Controls.Add(this.nudThickness);
+            this.Controls.Add(this.cbShowGrid);
+            this.Controls.Add(this.nudGridSize);
+            this.Controls.Add(this.btnColor2);
+            this.Controls.Add(this.btnColor1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbInput2);
+            this.Controls.Add(this.lblY1);
+            this.Controls.Add(this.btnGraph);
+            this.Controls.Add(this.tbInput1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintUpdate);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.nudGridSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampleRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRange)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput1;
+        private System.Windows.Forms.Button btnGraph;
+        private System.Windows.Forms.Label lblY1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput2;
+        private System.Windows.Forms.Button btnColor1;
+        private System.Windows.Forms.Button btnColor2;
+        private System.Windows.Forms.NumericUpDown nudGridSize;
+        private System.Windows.Forms.CheckBox cbShowGrid;
+        private System.Windows.Forms.NumericUpDown nudThickness;
+        private System.Windows.Forms.NumericUpDown nudSampleRate;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Button btnColor3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInput3;
+        private System.Windows.Forms.NumericUpDown nudRange;
+        private System.Windows.Forms.CheckBox cbRangeAuto;
+    }
+}
+
---
+++ GraphingCalc/GraphingCalc/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NCalc;
+using System.Threading;
+
+namespace GraphingCalc
+{
+    public partial class Form1 : Form
+    {
+        // Global Variables
+        double scaleFactor;
+        double maxVal = 50;
+        double minVal = -50;
+        double maxXDraw;
+        double[] origin = { 500, 500 };
+        double sampleRate = 0.3;
+        bool showGrid = true;
+        int gridIncrement = 5;
+        int thickness = 4;
+        string[] equations = new string[3];
+        Color[] palette = { Color.FromArgb(211, 19, 19), Color.FromArgb(19, 108, 211), Color.FromArgb(22, 210, 18) };
+        Point lastPoint;
+        Point mousePos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseWheel += new MouseEventHandler(Zoom);
+        }
+
+        private void PaintUpdate(object sender, PaintEventArgs e)
+        {
+            if (scaleFactor == 0) {
+                scaleFactor = 20;
+            }
+
+            maxXDraw = 0.75 * this.Width;
+
+            equations[0] = tbInput1.Text;
+            equations[1] = tbInput2.Text;
+            equations[2] = tbInput3.Text;
+
+            Brush aBrush = (Brush)Brushes.Black;
+            Graphics g = this.CreateGraphics();
+
+
+            GraphAxis();
+        }
+
+        public void CalcData()
+        {
+            double oldSampleRate = sampleRate;
+
+            for (int i = 0; i < equations.Length; i++) {
+                for (double x = minVal; x < maxVal; x += sampleRate) {
+                    try {
+                        if (equations[i] != "") {
+                            if (x < 0.3 && x > -0.3) {
+                                sampleRate = 0.005;
+                            } else {
+                                sampleRate = oldSampleRate;
+                            }
+
+                            double y = Function(x, equations[i]);
+                            if (!Double.IsNaN(y)) {
+                                GraphPoint(x, y, i);
+                            }   
+                        }
+                    } catch {
+
+                    }
+                } lastPoint = new Point();
+            }
+        }
+
+        private double Function(double x, string input)
+        {
+            do {
+                input = input.Replace("xx", "x*x");
+            } while (input.Contains("xx"));
+            input = input.Replace("x", "[x]");
+            input = input.Replace("pi", Math.PI.ToString());
+            input = input.Replace("e", Math.E.ToString());
+
+            try {
+                Expression e = new Expression(input, EvaluateOptions.IgnoreCase);
+
+
+                if (x == 0) {
+                    if (Function(x - 0.001, input) > Function(x - 0.002, input)) {
+                        return (double)float.MaxValue;
+                    } else {
+                        return (double)float.MinValue; ;
+                    }
+                }
+
+                try {
+                    e.Parameters["x"] = x;
+                    return Double.Parse(e.Evaluate().ToString());
+                } catch (DivideByZeroException) {
+                    return 10000;
+                } catch {
+                    return Double.NaN;
+                }
+            } catch {
+                return Double.NaN;
+            }
+            
+        }
+
+        private void GraphPoint(double x, double y, int i)
+        {
+            Brush aBrush = new SolidBrush(palette[i]);
+            Pen aPen = new Pen(aBrush, thickness);
+            Graphics g = this.CreateGraphics();
+            double xPrime = (scaleFactor * x) + origin[0];
+            double yPrime = (-scaleFactor * y) + origin[1];
+
+            try {
+                if (xPrime < maxXDraw) g.FillRectangle(aBrush, (float)xPrime, (float)yPrime, 1, 1);
+                else lastPoint = new Point();
+            } catch (OverflowException) {
+                if (xPrime < maxXDraw) {
+                    if (y < 0) {
+                        g.FillRectangle(aBrush, (float)xPrime, -1073741760, 1, 1);
+                    } else {
+                        g.FillRectangle(aBrush, (float)xPrime, 1000000000, 1, 1);
+                    }
+                } else lastPoint = new Point();
+            }
+            try {
+                if (!lastPoint.IsEmpty || lastPoint.X > xPrime) g.DrawLine(aPen, lastPoint, new Point((int)xPrime, (int)yPrime));
+            } catch (OverflowException) {
+                
+            }
+
+            lastPoint = new Point((int)xPrime, (int)yPrime);
+        }
+
+        private void GraphAxis()
+        {
+            Pen aPen = new Pen(Color.FromArgb(69, 73, 76), thickness);
+            Pen bPen = new Pen(Color.FromArgb(69, 73, 76), 1);
+            Graphics g = this.CreateGraphics();
+
+            g.DrawLine(aPen, new Point(0, (int)origin[1]), new Point((int)maxXDraw, (int)origin[1]));
+            if (origin[0] < maxXDraw) {
+                g.DrawLine(aPen, new Point((int)origin[0], 0), new Point((int)origin[0], 1000));
+            }
+
+            g.DrawLine(aPen, new Point((int)maxXDraw, 0), new Point((int)maxXDraw, 1000));
+
+            if (showGrid) {
+                for (int i = 0; i < maxVal; i += gridIncrement) {
+                    if ((int)(origin[0] + (i * scaleFactor)) < maxXDraw) {
+                        g.DrawLine(bPen, new Point((int)(origin[0] + (i * scaleFactor)), 0), new Point((int)(origin[0] + (i * scaleFactor)), 1000));
+                    }
+                    g.DrawLine(bPen, new Point(0, (int)(origin[1] + (i * scaleFactor))), new Point((int)maxXDraw, (int)(origin[1] + (i * scaleFactor))));
+                }
+
+                for (int i = 0; i > minVal; i -= gridIncrement) {
+                    if ((int)(origin[0] + (i * scaleFactor)) < maxXDraw) {
+                        g.DrawLine(bPen, new Point((int)(origin[0] + (i * scaleFactor)), 0), new Point((int)(origin[0] + (i * scaleFactor)), 1000));
+                    }
+                    g.DrawLine(bPen, new Point(0, (int)(origin[1] + (i * scaleFactor))), new Point((int)maxXDraw, (int)(origin[1] + (i * scaleFactor))));
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePos = this.PointToClient(Cursor.Position);
+        }
+
+        private void UpdateDraw()
+        {
+            lastPoint = new Point();
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            GraphAxis();
+            CalcData();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            origin[0] += this.PointToClient(Cursor.Position).X - mousePos.X;
+            origin[1] += this.PointToClient(Cursor.Position).Y - mousePos.Y;
+            UpdateDraw();
+        }
+
+        private void Zoom(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0) {
+                scaleFactor *= 2;
+            } else {
+                scaleFactor /= 2;
+            }
+            
+            UpdateDraw();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            maxXDraw = 0.75 * this.Width;
+            UpdateDraw();
+        }
+
+        private void btnGraph_Click(object sender, EventArgs e)
+        {
+            UpdateDraw();
+        }
+        private void colorSelect(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            ColorDialog color = new ColorDialog();
+            color.ShowDialog();
+            palette[int.Parse(b.Tag.ToString())] = color.Color;
+            b.BackColor = color.Color;
+            UpdateDraw();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnColor1.BackColor = palette[0];
+            btnColor2.BackColor = palette[1];
+            btnColor3.BackColor = palette[2];
+
+            origin[0] = 0.375 * this.Width;
+            origin[1] = 0.5 * this.Height;
+        }
+
+        private void nudGridSize_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudGridSize.Value <= 0) {
+                nudGridSize.Value = 1;
+            }
+
+            gridIncrement = (int)Math.Floor(nudGridSize.Value);
+            UpdateDraw();
+        }
+
+        private void cbShowGrid_Click(object sender, EventArgs e)
+        {
+            if (cbShowGrid.Checked) {
+                showGrid = true;
+            } else {
+                showGrid = false;
+            }
+
+            UpdateDraw();
+        }
+
+        private void nudThickness_ValueChanged(object sender, EventArgs e)
+        {
+            thickness = (int)nudThickness.Value;
+            nudThickness.Refresh();
+            UpdateDraw();
+        }
+
+        private void nudSampleRate_ValueChanged(object sender, EventArgs e)
+        {
+            sampleRate = (double)nudSampleRate.Value;
+
+            nudSampleRate.Refresh();
+            UpdateDraw();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            double valAtX = (this.PointToClient(Cursor.Position).X - origin[0]) / scaleFactor;
+            double valAtY = Function(valAtX, equations[0]);
+
+            lblX.Text = valAtX.ToString();
+            lblY.Text = valAtY.ToString();
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            origin[0] = 0.375 * this.Width;
+            origin[1] = 0.5 * this.Height;
+            scaleFactor = 20;
+
+            UpdateDraw();
+        }
+
+        private void nudRange_ValueChanged(object sender, EventArgs e)
+        {
+            if (!cbRangeAuto.Checked) {
+                maxVal = (int)nudRange.Value;
+                minVal = -maxVal;
+            }
+
+            UpdateDraw();
+        }
+
+        private void autoRange(object sender, EventArgs e)
+        {
+            if (cbRangeAuto.Checked) {
+                maxVal = maxXDraw / scaleFactor;
+                minVal = -maxVal;
+            } else {
+                maxVal = (int)nudRange.Value;
+                minVal = -maxVal;
+            }
+        }
+    }
+}
